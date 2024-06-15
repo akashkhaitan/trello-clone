cc030f942085411449469637125fc11441a6aefd
Akash Khaitan <akashkhaitan@gmail.com>
2024-06-15 17:42:59 +0530

    feat: created store and used in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+<script setup>
+import { useBoardStore } from "~/stores/boardStore";
+
+const boardStore = useBoardStore();
+</script>
 
 <template>
   <h1>Trello Clone</h1>
+  <pre>{{ boardStore.board }}</pre>
 </template>
